aa43cb12efe889f570740b0fc34a0b3124ddb938
Devon Wolfkiel <devon.wolfkiel@hingehealth.com>
2023-05-19 16:48:34 -0700

    remove dogs controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DogsController } from './dogs/dogs.controller';
 import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController, DogsController],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
